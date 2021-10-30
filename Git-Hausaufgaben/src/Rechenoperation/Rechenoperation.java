e57a6d3dff2febc98cedb599a2e8aca2bb396b96
ZyklopRT <56200986+ZyklopRT@users.noreply.github.com>
2021-10-30 17:10:11 +0200

    added Homework Rechenoperation with javadoc, finished

---
+++ Git-Hausaufgaben/src/Rechenoperation/Rechenoperation.java
@@ -0,0 +1,17 @@
+package Rechenoperation;
+/**
+ * Interface Rechenoperation
+ * 
+ * @author Justin, Jonas, Jan
+ * @version 1.0.0
+ */
+public interface Rechenoperation {
+	
+	/**
+	 * Methode zum Berechnen nach eigener Art
+	 * 
+	 * @param x > double
+	 * @return double > Gibt berechneten Wert zurück
+	 */
+	public double berechne(double x);
+}
