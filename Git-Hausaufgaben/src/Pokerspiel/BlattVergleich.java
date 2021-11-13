2c920751f22988dc67b9c8cf2bf6bac9ae66f29a
ZyklopRT <56200986+ZyklopRT@users.noreply.github.com>
2021-11-13 19:24:32 +0100

    [HA06#Finished] Pokerspiel -Javadoc Kommentare hinzugefügt

--- Git-Hausaufgaben/src/Pokerspiel/BlattVergleich.java
+++ Git-Hausaufgaben/src/Pokerspiel/BlattVergleich.java
@@ -1,10 +1,30 @@
 package Pokerspiel;
 
-import java.util.Arrays;
 import java.util.Comparator;
 
+/**
+ * Klasse BlattVergleich implementiert Comparator<Blatt> zum Vergleichen vom Objekte des Types Blatt
+ * 
+ * @author Justin, Jonas, Jan
+ * @version 1.0.0
+ *
+ */
 public class BlattVergleich implements Comparator<Blatt> {
-
+	
+	/**
+	 * Vergleicht die beiden angegebenen Parameter mit einander
+	 * 
+	 * @param blatt1 > Objekt vom Typ Blatt
+	 * @param blatt2 > Objekt vom Typ Blatt
+	 * @return int > Vergleich-Ergebniss
+	 * <br>
+	 *		>1 = Blatt1 größer
+	 * <br>
+	 *		0  = gleich groß
+	 * <br>
+	 *		<1 = Blatt2 größer
+	 *
+	 */
 	public int compare(Blatt blatt1, Blatt blatt2) {
 
 		int[] b1Valence = blatt1.getValance();
@@ -17,22 +37,20 @@ public class BlattVergleich implements Comparator<Blatt> {
 		
 		// Valance check
 		if(b1Valence[0] != b2Valence[0]) {
-			return b1Valence[0]-b2Valence[0]; // greater then 0 = blatt1 gewinnt, less then  = blatt2 gewinnt
+			return b1Valence[0]-b2Valence[0];
 		} else {
-			// same valance
-			// check if some valance value is higher
 			if(b1Valence[1] != b2Valence[1]) {
-				return b1Valence[1]-b2Valence[1]; // greater then 0 = blatt1 gewinnt, less then 0 = blatt2 gewinnt
+				return b1Valence[1]-b2Valence[1]; 
 			} 
 			else if(b1Valence[0] == 2){
-				return b1HighestValue-b2HighestValue; // greater then 0 = blatt1 gewinnt, less then 0 = blatt2 gewinnt
+				return b1HighestValue-b2HighestValue; 
 			}
 			else if(b1Valence[1] == 0) {
 				return b1ValueTotal-b2ValueTotal;
 			}
 		}
 	
-		return 0; // Draw, no winner
+		return 0;
 
 	}
 	
